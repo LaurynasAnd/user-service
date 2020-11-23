e132c5d726f230a42159d827768030c78c6f7264
LaurynasAnd <l.andrulionis@gmail.com>
2020-11-23 13:54:49 +0200

    slamm code changes

--- src/Service/DataService.php
+++ src/Service/DataService.php
@@ -19,15 +19,7 @@ class DataService
             'GET',
             'http://127.0.0.1:7080/api/data'
         );
-
-        // $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        // $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        // $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
         $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
         return $content;
     }
